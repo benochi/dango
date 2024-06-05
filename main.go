ad10da66bae5135a05ba33cdf37740f254867bd9
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-06-05 11:15:53 -0500

    fix testing for deprecation.

--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", pingHandler)
-	http.ListenAndServe(":5027", nil)
+	err := http.ListenAndServe(":5027", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{"message": "Pinging the server"}
-	jsonResponse, _ := json.Marshal(response)
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResponse)
 }
--- main_test.go
+++ main_test.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"testing"
 )
 
 func TestPingHandler(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/ping")
+	resp, err := http.Get("http://localhost:5027/ping")
 	if err != nil {
 		t.Errorf("Failed to send request: %v", err)
 		return
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Errorf("Failed to read response body: %v", err)
 		return
